a28ed49070e06b52ff8cdd9d1e0d056db5c1a13c
bobred <bobred@sky.com>
2026-06-04 09:53:20 +0100

    Refactor setup.py for improved readability

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 
 lib_folder = os.path.dirname(os.path.realpath(__file__))
 requirement_path = f"{lib_folder}/requirements.txt"
-install_requires = [] # Here we'll add: ["gunicorn", "docutils>=0.3", "lxml==0.5a7"]
+
 if os.path.isfile(requirement_path):
     with open(requirement_path) as f:
         install_requires = f.read().splitlines()
